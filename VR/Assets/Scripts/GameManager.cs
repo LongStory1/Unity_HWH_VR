59cbc6335f9a899861f56b966de735e85991de73
LongStory1 <61855966+LongStory1@users.noreply.github.com>
2020-04-10 15:35:42 +0800

    完成並發布 APK

--- VR/Assets/Scripts/GameManager.cs
+++ VR/Assets/Scripts/GameManager.cs
@@ -13,6 +13,38 @@ public class GameManager : MonoBehaviour
     public GameObject groupLight;
     [Header("會動的寶箱")]
     public Transform chest;
+    [Header("喇叭")]
+    public AudioSource aud;
+    [Header("木板上滑動音效")]
+    public AudioClip soundWoodMove;
+    [Header("敲門音效")]
+    public AudioClip soundKnock;
+    [Header("開門音效")]
+    public AudioClip soundOpen;
+    [Header("門的動畫控制器")]
+    public Animator aniDoor;
+
+    private int countDoor; //看到門的次數
+
+    public int countChest; //看到寶箱的次數
+    /// <summary>
+    ///看到門
+    /// </summary>
+    public void LookDoor()
+    {
+        countDoor++;     //遞增 1
+
+        //如果 看到的門次數 等於 1
+        if (countDoor == 1)
+        {
+            aud.PlayOneShot(soundKnock, 5);
+        }
+        else if (countDoor == 2)
+        {
+            aud.PlayOneShot(soundOpen, 4.5f);
+            aniDoor.SetTrigger("開門觸發器");
+        }
+    }
 
     //定義方法 (Method):有特定內容的功能
     //修飾詞 傳回類型 方法名稱(){敘述}
@@ -52,19 +84,22 @@ public class GameManager : MonoBehaviour
     /// <returns></returns>
     public IEnumerator MoveChest()
     {
-        //前:forward
-        //右:right
-        //上:up
-        //for 迴圈(初始值，條件，跌代器 - 每次迴圈結束要執行的敘述)
-        for (int i = 0; i < 25; i++)
-        {
-            chest.position -= chest.forward * 0.1f;            //寶箱.座標 遞減 寶箱.前方
-            yield return new WaitForSeconds(0.001f);
-        }
         //GetComponent<泛型>()取得元件:可以取得物件在屬性面板上的所有元件
         //enable 元件啟動或停止 true 啟動 false 停止
         chest.GetComponent<CapsuleCollider>().enabled = false;
 
+        //前:forward
+        //右:right
+        //上:up
+        //for 迴圈(初始值，條件，跌代器 - 每次迴圈結束要執行的敘述)
+        for (int i = 0; i < 10; i++)
+        {
+            chest.position -= chest.forward * 0.3f;            //寶箱.座標 遞減 寶箱.前方
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        //喇叭.播放一次音效(音效，音量)
+        aud.PlayOneShot(soundWoodMove, 2.5f);
     }
     //事件:開始 - 播放時執行一次 ，初始化或遊戲開始需要的內容
     private void Start()
